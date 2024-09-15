1218d969aea594898ae3fc2002d2e9d9afbdbab6
qwaserand <andres.emanuel.89@gmail.com>
2024-09-15 01:41:18 -0300

    agregado de profiles

--- src/main/java/com/qwaserand/preguntados/entity/Pregunta.java
+++ src/main/java/com/qwaserand/preguntados/entity/Pregunta.java
@@ -2,33 +2,27 @@ package com.qwaserand.preguntados.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-@Table(name = "pregunta")
+@Table(name = "pregunta", uniqueConstraints = { @UniqueConstraint(columnNames = "enunciado") })
 public class Pregunta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_pregunta")
+    @Column(name = "pregunta_id")
     private long id;
 
     @Column(name = "statement", nullable = false)
     private String enunciado;
 
-    /*----------------------------------------------------------------------------*/
+    @ManyToOne // join columns van donde esta FK
+    @JoinColumn(name = "categoria_id", referencedColumnName = "categoria_id")
+    private Categoria categoria;
 
-    public Pregunta() {
-    }
-
-    /**
-     * @param id
-     * @param enunciado
-     * */
-    public Pregunta(long id, String enunciado) {
-        this.id = id;
-        this.enunciado = enunciado;
-    }
-
-    /*----------------------------------------------------------------------------*/
+    @OneToMany(mappedBy = "pregunta", cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+    private List<Respuesta> opciones = new ArrayList<>();
 
     public long getId() {
         return id;
@@ -46,6 +40,24 @@ public class Pregunta {
         this.enunciado = enunciado;
     }
 
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+        this.categoria.agregarPregunta(this);
+    }
+
+    public List<Respuesta> getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(List<Respuesta> opciones) {
+        this.opciones = opciones;
+    }
+
     public void agregarRespuesta(Respuesta respuesta) {
+        this.opciones.add(respuesta);
     }
 }
